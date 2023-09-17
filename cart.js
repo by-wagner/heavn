101d21ee4042ff57b52cd52cd7065baf8b32aa80
wagner <wagnertechdev@gmail.com>
2023-09-17 16:18:50 -0300

    first commit

---
+++ cart.js
@@ -0,0 +1,75 @@
+const cards = document.querySelectorAll(".card");
+
+let count = 0;
+
+cards.forEach((card) => {
+  // find the add-to-cart button
+  const addToCartBtn = card.querySelector(".add-to-cart");
+
+  // add a click event listener to the add-to-cart button
+  addToCartBtn.addEventListener("click", () => {
+    const counterElement = document.getElementById("cart-count-nav");
+    count++;
+    counterElement.innerHTML = count;
+  });
+});
+
+const cartItems = [];
+
+function updateCart(product) {
+  const counterElement = document.getElementById("cart-count-nav");
+  count++;
+  counterElement.innerHTML = count;
+
+  let existingProduct = cartItems.find((p) => p.id === product.id);
+
+  if (existingProduct) {
+    existingProduct.quantity += 1;
+  } else {
+    product.quantity = 1;
+    cartItems.push(product);
+  }
+
+  const cartItemsList = document.querySelector(".cart-items ul");
+  cartItemsList.innerHTML = "";
+  cartItems.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.classList =
+      "list-group-item d-flex justify-content-between align-items-center";
+    listItem.innerHTML = `${item.name} <span class="badge badge-primary badge-pill">${item.quantity}</span>`;
+    cartItemsList.appendChild(listItem);
+  });
+}
+
+fetch("products.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    listProducts(data.products);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+function listProducts(productlist) {
+  const listElement = document.getElementById("card-list");
+
+  productlist.forEach((item) => {
+    const listItem = document.createElement("div");
+    listItem.classList = "col-md-6 col-lg-3 mb-4";
+    listItem.innerHTML = `     
+    <div class="card card-shadow border-0" data-id="Deck-1">
+        <img src="${item.image}" class="card-img-top rounded-0"
+            alt="Product 1">
+        <div class="card-body">
+            <h5 class="card-title mb-0">${item.name}</h5>
+            <p class="card-text mb-2">${item.price}</p>
+            <button class="btn btn-dark btn-block add-to-cart" data-product='${JSON.stringify(
+              item
+            )}'>Add to Cart</button>
+        </div>
+    </div>
+`;
+    listElement.appendChild(listItem);
+  });
+}
---
+++ main.js
@@ -0,0 +1,84 @@
+const productsArray = [
+  { products: "boards", numberProducts: 2 },
+  { products: "apparel", numberProducts: 2 },
+];
+
+function selectProducts(criteria) {
+  return productsArray.filter((product) => product[criteria] > 0);
+}
+
+const name = productsArray.map((o) => o.name);
+console.log(productsArray);
+// [ 'decks', 'apparel' ]
+const numberProdutcs = productsArray.map((o) => o.number);
+// [ '2', '2' ]
+
+const nameProducts = [
+  {
+    name: "Arctic Mist",
+    description: "Deck size 5'3",
+    price: "$65.00",
+  },
+  {
+    name: "Obsidian Noir",
+    description: "Deck size 5'3",
+    price: "$65.00",
+  },
+  {
+    name: "Rebel Rider Backpack",
+    description: "leather backpack",
+    price: "$450.00",
+  },
+  {
+    name: "Rider's Denim Jacket",
+    description: "Denim Jacket",
+    price: "$240.00",
+  },
+];
+
+const emailInput = document.querySelector("#newsletter1");
+const subscribeBtn = document.querySelector("#subscribeButton");
+
+subscribeBtn.addEventListener("click", function () {
+  const email = emailInput.value;
+
+  let emailList = JSON.parse(localStorage.getItem("emailList")) || [];
+
+  if (emailList.includes(email)) {
+    swal("Oops!", "This email is already subscribed!", "error");
+  } else {
+    emailList.push(email);
+    localStorage.setItem("emailList", JSON.stringify(emailList));
+    let count = emailList.length;
+    swal({
+      title: "Nice!",
+      text: `Thank you for subscribing! You now have ${count} emails registered.`,
+      icon: "success",
+      buttons: ["Cancel", "Clear List"],
+    }).then((value) => {
+      if (value) {
+        emailList = [];
+        localStorage.setItem("emailList", JSON.stringify(emailList));
+        const emailListElement = document.querySelector("#emailListDisplay");
+        emailListElement.innerHTML = "";
+      }
+    });
+  }
+});
+
+let emailList = JSON.parse(localStorage.getItem("emailList")) || [];
+
+const emailListElement = document.querySelector("#newsletter1");
+emailListElement.innerHTML = emailList
+  .map((email) => `<li>${email}</li>`)
+  .join("");
+
+// Pause the video when the user scrolls the page.
+jQuery(window).scroll(function () {
+  var height = jQuery(window).scrollTop();
+  if (height > 0) {
+    jQuery("#my-video")[0].pause();
+  } else {
+    jQuery("#my-video")[0].play();
+  }
+});
